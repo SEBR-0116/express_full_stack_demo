1983c97d704f53404ff05d6639a0120b2bc3edb4
Jeremy <Taubman33@gmail.com>
2024-02-15 10:14:44 -0500

    button refactoring

--- BackEnd/dogController.js
+++ BackEnd/dogController.js
@@ -1,12 +1,13 @@
 const dogs = [
-    {name: "lassie", 
-    image:'https://media.istockphoto.com/id/1294195550/photo/shetland-sheepdog-on-mountain.jpg?s=612x612&w=0&k=20&c=l0X9LzEVlFxTJ-eNbPnYXwFw_OPGUMM_lNsEjcia_ZY='},
+  
     {name: "balto",
     image:'https://statcdn.fandango.com/MPX/image/NBCU_Fandango/701/579/thumb_0232B08C-6AF3-499A-8E07-975D8A4593E0.jpg'},
     {name: "air bud", 
-    image: "www.dogs.com/airbud"},
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvIGPgSvRzk1-HBqDKQ2JnbP4F8SnavVY2Kw&usqp=CAU"},
+    {name: "lassie", 
+    image:'https://media.istockphoto.com/id/1294195550/photo/shetland-sheepdog-on-mountain.jpg?s=612x612&w=0&k=20&c=l0X9LzEVlFxTJ-eNbPnYXwFw_OPGUMM_lNsEjcia_ZY='},
     {name: "wishbone",
-    image:"www.dogs.com/wishbone"}
+    image:"https://images2.minutemediacdn.com/image/upload/c_fill,w_1440,ar_16:9,f_auto,q_auto,g_auto/shape/cover/sport/wishbonehed-fd9cc8610ebcd6fc82ad71a223c593cf.png"}
 ]
 
 const getDogs = (req, res) => {
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,14 +1,18 @@
 console.log('working')
-const garfButt = document.querySelector('#garfield')
-const cheshButt = document.querySelector('#cheshire')
-const salButt = document.querySelector('#salem')
+
 const allCats = document.querySelector('#allCats')
-const lassie = document.querySelector('#lassie')
-const baltoButt = document.querySelector('#balto')
 const alldogs = document.querySelector('#allDogs')
+const catButtons = document.querySelectorAll('.cat')
+const dogButtons = document.querySelectorAll('.dog')
+const nameList = document.querySelector("#names")
+const dataContainer = document.querySelector("#data")
 
-
+//gets a list of 4 of our cats
 allCats.addEventListener('click', async ()=> {
+    const nameList = document.querySelector("#names")
+    const dataContainer = document.querySelector("#data") 
+     dataContainer.style.display="none"
+     nameList.style.display="block"
     const response = await axios.get('http://localhost:3001/cats/')
     console.log(response.data)
     let catName1 = document.querySelector('#catName1')
@@ -19,13 +23,14 @@ allCats.addEventListener('click', async ()=> {
     catName3.innerText = response.data[4].name
     let catName4 = document.querySelector('#catName4')
     catName4.innerText = response.data[6].name
-
-
 })
 
-
-
+//gets a list of 4 of our dogs
 alldogs.addEventListener('click', async ()=> {
+    const nameList = document.querySelector("#names")
+   const dataContainer = document.querySelector("#data") 
+    dataContainer.style.display="none"
+    nameList.style.display="block"
     const response = await axios.get('http://localhost:3001/dogs/')
     console.log(response.data)
     let catName1 = document.querySelector('#catName1')
@@ -33,26 +38,48 @@ alldogs.addEventListener('click', async ()=> {
     let catName2 = document.querySelector('#catName2')
     catName2.innerText = response.data[1].name
     let catName3 = document.querySelector('#catName3')
-    catName3.innerText = response.data[4].name
+    catName3.innerText = response.data[2].name
     let catName4 = document.querySelector('#catName4')
-    catName4.innerText = response.data[6].name
-
-
+    catName4.innerText = response.data[3].name
 })
 
 
-
-const getButtons = document.querySelectorAll('.cat')
-getButtons.forEach((button) => {
+//pulls individual Cat data
+catButtons.forEach((button) => {
     button.addEventListener("click", async ()=> {
-        console.log(button.value)
         const response = await axios.get(`http://localhost:3001/cats/${button.value}`)
+    console.log(response.data)
+    const nameList = document.querySelector("#names")
+    const dataContainer = document.querySelector("#data") 
+     dataContainer.style.display="block"
+     nameList.style.display="none"
+    let catName = document.querySelector('#name')
+    let catImg = document.querySelector('#img')
+    catName.innerText = response.data.name
+    catImg.setAttribute('src', response.data.image)
+    let catNames = document.querySelector('#names')
+    catNames.style.display = 'none'
+    }
+    )
+})
+
+
+//pulls individual dog data
+
+    dogButtons.forEach((button) => {
+    console.log('clicked')
+    button.addEventListener("click", async ()=> {
+        const nameList = document.querySelector("#names")
+        const dataContainer = document.querySelector("#data") 
+         dataContainer.style.display="block"
+         nameList.style.display="none"
+        const response = await axios.get(`http://localhost:3001/dogs/${button.value}`)
     console.log(response.data)
     let catName = document.querySelector('#name')
     let catImg = document.querySelector('#img')
     catName.innerText = response.data.name
     catImg.setAttribute('src', response.data.image)
-    let catNames = document.querySelector('#catnames')
+    let catNames = document.querySelector('#names')
     catNames.style.display = 'none'
     }
     )
@@ -60,50 +87,3 @@ getButtons.forEach((button) => {
 
 
 
-
-
-// garfButt.addEventListener('click', async ()=> {
-//     const response = await axios.get('http://localhost:3001/cats/1')
-//     console.log(response.data)
-//     let catName = document.querySelector('#name')
-//     let catImg = document.querySelector('#img')
-//     catName.innerText = response.data.name
-//     catImg.setAttribute('src', response.data.image)
-//     let catNames = document.querySelector('#catnames')
-//     catNames.style.display = 'none'
-
-// })
-
-// baltoButt.addEventListener('click', async ()=> {
-//     const response = await axios.get('http://localhost:3001/dogs/1')
-//     console.log(response.data)
-//     let catName = document.querySelector('#name')
-//     let catImg = document.querySelector('#img')
-//     catName.innerText = response.data.name
-//     catImg.setAttribute('src', response.data.image)
-//     let catNames = document.querySelector('#catnames')
-//     catNames.style.display = 'none'
-
-// })
-
-
-// cheshButt.addEventListener('click', async ()=> {
-//     const response = await axios.get('http://localhost:3001/cats/6')
-//     console.log(response.data)
-//     let catName = document.querySelector('#name')
-//     let catImg = document.querySelector('#img')
-//     catName.innerText = response.data.name
-//     catImg.setAttribute('src', response.data.image)
-
-// })
-
-
-// lassie.addEventListener('click', async ()=> {
-//     const response = await axios.get('http://localhost:3001/dogs/0')
-//     console.log(response.data)
-//     let catName = document.querySelector('#name')
-//     let catImg = document.querySelector('#img')
-//     catName.innerText = response.data.name
-//     catImg.setAttribute('src', response.data.image)
-
-// })
